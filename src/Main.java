8da9f3d337f79224f2cc8e2edb728862f822f4fe
Michail Michailidis <mmichaelid@gmail.com>
2013-11-15 17:25:56 -0500

    I print something for visualization

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,16 @@ public class Main {
 		// loading training data for ones
 		Vector<Digit> trainingTwos = filesToDigitsVector("digits/2-training/",12);
 		// loading testing data 
-		Vector<Digit> testingData = filesToDigitsVector("digits/testing/",40);
+		//Vector<Digit> testingData = filesToDigitsVector("digits/testing/",40);
 		
 		//given class is 0 count the pixels that are on
 		byte[][] countsForDigitZero = countPixelOccurencies(trainingZeros);
+		
+		//TAO can you visualize this? I guess you could see what is the max
+		// and have colors between 0 and max --> convert it to 0-255 let's say
+		Digit dummy = new Digit(1,countsForDigitZero);
+		System.out.println(dummy); // see what is printed here (80x80)
+		
 		
 		//given class is 2 count the pixels that are on
 		byte[][] countsForDigitTwo = countPixelOccurencies(trainingTwos);
